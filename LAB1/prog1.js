26afd6a94711b48ab35c2673d4d321e005dc6334
karim Mohamed <karimdesouki147@gmail.com>
2023-12-26 16:11:19 +0200

    organized

---
+++ LAB1/prog1.js
@@ -0,0 +1,7 @@
+var firstName =prompt("what is your firstname ?");
+var lastName=prompt("what is your Lastname ?");
+confirm(`Your Full Name is ${firstName} ${lastName} ?`)
+var birthyear=prompt("enter your age ?");
+
+var fullName=document.write(` Welcome  ${firstName}   ${lastName}   you are   ${birthyear}  years old <br>`);
+
--- LAB1/prog2.js
+++ LAB1/prog2.js
@@ -1,10 +1,6 @@
-var firstName =prompt("what is your firstname ?");
-var lastName=prompt("what is your Lastname ?");
-var birthyear=prompt("enter your age ?");
-var fullName=document.write(" Welcome ", firstName + " " + lastName + " you are " + birthyear , " years old <br>");
-
-
 document.write('it\'s the first release of a calculator that only has a summation feature.')
+
 var firstNumber=Number(prompt("enter the first number" , firstNumber));
 var secondNumber=Number(prompt("enter the second number" , secondNumber)); 
+
 document.write("<br> the sum of the two numbers is:", firstNumber + secondNumber);
